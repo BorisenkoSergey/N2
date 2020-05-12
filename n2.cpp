1cecff19a485f0e76cf993b185ee488375d380de
Sergey Borisenko <61838037+BorisenkoSergey@users.noreply.github.com>
2020-05-12 16:11:22 +0300

    *5
    Тут я создаю цикл в цикле, для того, чтобы программа проверила все возможные(подходящие условию) пары чисел, и отобрав подходящую пару с самой большой суммой , присвоило значение ее суммы переменной max

--- n2.cpp
+++ n2.cpp
@@ -14,4 +14,16 @@ int main()
     array1[i] = m;
   }
   int max = 0;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = i + 1; j < n; j++)
+    {
+      int p = array1[i] * array1[j];
+      int s = array1[i] + array1[j];
+      if (s % 7 == 0 && p % 3 == 0 && s > max)
+      {
+        max = s;
+       }
+     }
+  }
 }
